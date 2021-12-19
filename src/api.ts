694b941820a28a6f3af8c032e37d97151274b5f8
HyungJin <hyungjin0710@gmail.com>
2021-12-19 19:59:46 +0900

    #8.5 Home Screen part One

---
+++ src/api.ts
@@ -0,0 +1,8 @@
+const API_KEY = "6b206f5fd811dfe635d8704a68605dfa";
+const BASE_PATH = "https://api.themoviedb.org/3";
+
+export function getMovies() {
+  return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
+    (response) => response.json()
+  );
+}
