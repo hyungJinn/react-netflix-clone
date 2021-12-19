f6286c7d731af3742403581daf6199fc7a2761a1
HyungJin <hyungjin0710@gmail.com>
2021-12-19 20:50:40 +0900

    #8.6 Home Screen part Two

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,25 @@
 const API_KEY = "6b206f5fd811dfe635d8704a68605dfa";
 const BASE_PATH = "https://api.themoviedb.org/3";
 
+interface Imovie {
+  backdrop_path: string;
+  id: number;
+  overview: string;
+  poster_path: string;
+  title: string;
+}
+
+export interface IGetMoviesResult {
+  dates: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: Imovie[];
+  total_pages: number;
+  total_results: number;
+}
+
 export function getMovies() {
   return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
     (response) => response.json()
